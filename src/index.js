cb772c24fb66bbe168f4e816df70c9ea53ae89ac
shawn·black <longshanzhiyu_2018@163.com>
2022-09-21 14:54:10 +0800

    添加列表

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TodoList from './TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
